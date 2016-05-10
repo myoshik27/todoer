64388352feda2eee3bb5adc0a197cf1e82cea5b8
Matt Yoshihara <mkyoshihara@gmail.com>
2016-05-10 16:46:11 -0700

    added taskServerModel

---
+++ server/models/taskServerModel.js
@@ -0,0 +1,14 @@
+// this is the task.js file located at server/models/task.js
+// when run, this file will create the schema Tasks in our mongodb
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var TaskSchema = new mongoose.Schema({
+	user: {type: Schema.Types.ObjectId, ref: "User", required:true},
+	task: {type: String, required: true},
+	created_at: {type: Date, default: Date.now, required:true},
+	updated_at: {type: Date, default: Date.now, required:true}
+})
+
+mongoose.model("Task", TaskSchema);
