afee0409cebc5c8cda2e7856443aa484d8ff86e5
Matt Yoshihara <mkyoshihara@gmail.com>
2016-05-10 23:18:19 -0700

    added showUser method

--- server/controllers/usersServerCtrl.js
+++ server/controllers/usersServerCtrl.js
@@ -3,3 +3,17 @@
 
 var mongoose = require('mongoose');
 var User = mongoose.model("User");
+
+module.exports = (function(){
+	return {
+		showUser: function(request, response){
+			User.find({}, function(errors, results){
+				if(errors){
+					console.log(errors);
+				} else{
+					response.json(results);
+				}
+			})
+		},
+	}
+})
