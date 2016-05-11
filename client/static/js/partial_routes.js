5f02e21f0eec5082035d65f7b9d0d3edbd8c527d
Matt Yoshihara <mkyoshihara@gmail.com>
2016-05-10 18:08:37 -0700

    started code for loading partials

---
+++ client/static/js/partial_routes.js
@@ -0,0 +1,10 @@
+console.log("Partial Routes Loaded");
+
+var todoer = angular.module('todoer',['ngRoute'])
+
+// todoer.config(function($routeProvider){
+// 	$routeProvider
+// 	.when('/',{
+// 		templateUrl: 
+// 	})
+// })
