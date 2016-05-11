0c7987980c24bdf6dd6de03e4a470bc0256b9b26
Matt Yoshihara <mkyoshihara@gmail.com>
2016-05-11 12:14:41 -0700

    added getUsers and getTasks routes

--- server/config/routes.js
+++ server/config/routes.js
@@ -5,16 +5,16 @@
 // this file will be called on by the main server.js file and it will be passed the app variable. 
 
 // the routes.js file must require all of the server controllers("the exact route from the base file") in order to call upon their functions
-
-/*
-
-var users = require(./../controllers/usersAngCtrl.js)
+console.log("Loaded Express Routes");
+var users = require("./../controllers/usersServerCtrl.js");
+var tasks = require("./../controllers/tasksServerCtrl.js");
 
 module.exports = function(app){
 	app.get('/getUsers', function(request, response){
-		users.show(request,response);
+		users.showUser(request,response);
+	})
+
+	app.get('/getTasks', function(request,response){
+		tasks.showTask(request,response);
 	})
 }
-
-
-*/
