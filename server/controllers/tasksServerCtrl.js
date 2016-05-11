5a0b684c2d995fc13df07fa96a9dea0b53c2fb89
Matt Yoshihara <mkyoshihara@gmail.com>
2016-05-11 12:16:17 -0700

    added parenthesis at the end

--- server/controllers/tasksServerCtrl.js
+++ server/controllers/tasksServerCtrl.js
@@ -1,19 +1,20 @@
 // Server Controllers will be called on by the routes.js file and make calls to the database
 // Server Controllers require mongoose and all models
-console.log("Loaded User Server Controller");
+console.log("Loaded Task Server Controller");
 var mongoose = require('mongoose');
-var User = mongoose.model("User");
+var Task = mongoose.model("Task");
 
 module.exports = (function(){
 	return {
-		showUser: function(request, response){
-			User.find({}, function(errors, results){
+		showTask: function(request, response){
+			Task.find({}, function(errors, results){
 				if(errors){
 					console.log(errors);
 				} else{
 					response.json(results);
 				}
 			})
-		},
+		}
+
 	}
 })();
