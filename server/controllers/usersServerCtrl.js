0cebf774c14c52071c359896557765cbd37ddbad
Matt Yoshihara <mkyoshihara@gmail.com>
2016-05-10 17:35:26 -0700

    required mongoose

--- server/controllers/usersServerCtrl.js
+++ server/controllers/usersServerCtrl.js
@@ -1,3 +1,5 @@
 // Server Controllers will be called on by the routes.js file and make calls to the database
 // Server Controllers require mongoose and all models
 
+var mongoose = require('mongoose');
+var User = mongoose.model("User");
