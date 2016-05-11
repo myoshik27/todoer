18179861627ae8a32825e2f0794d0dc76ad5b18d
Matt Yoshihara <mkyoshihara@gmail.com>
2016-05-10 23:17:12 -0700

    made edits

--- client/static/js/controllers/usersAngCtrl.js
+++ client/static/js/controllers/usersAngCtrl.js
@@ -1,8 +1,6 @@
 // Controller methods get called by partials and call on factory methods
 // Controller anonymous function may inject multiple factories if needed
 
-console.log("UsersController Loaded")
-
 
 todoer.controller("UsersController", function(UserFactory){
 	var _this = this;
